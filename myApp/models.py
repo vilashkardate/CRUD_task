0a6079b395c971c471e19d73e09f1981484f95b8
Vilash Kardate <vilashkardate@Vilashs-MacBook-Air.local>
2021-09-30 17:07:10 +0530

    first commit

---
+++ myApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+	eid = models.CharField(max_length=10)
+	ename = models.CharField(max_length=50)
+	eemail = models.EmailField()
+	econtact = models.CharField(max_length=10)
+
+	class Meta:
+		db_table = "employee"
+
+	def __str__(self):
+		return self.ename
